0bf74964cd04f8c44ce257ec4a70380f35cc00b7
Charlesasiedu <asieducharles899@gmail.com>
2022-12-19 18:50:07 -0800

    straings and arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,10 +5,12 @@
  * @s: this is the input string
  * Return: length of a string
  */
+
 int _strlen(char *s)
 {
 	int index;
 
 	for (index = 0; s[index] != '\0' ; index++)
+
 		return (index);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
+ *
  * @str: this is the input string
  */
 void _puts(char *str)

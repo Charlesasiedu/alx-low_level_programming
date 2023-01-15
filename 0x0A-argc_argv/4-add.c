9148a70e0df45927959a5ab7cb2b243bca06d64d
Charlesasiedu <asieducharles899@gmail.com>
2023-01-14 21:51:36 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,12 +23,11 @@ int check_num(char *str)
 		count++;
 	}
 	return (1);
-
+}
 /**
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- *
  *  Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -36,7 +35,7 @@ int main(int argc, char *argv[])
 	int count;
 	int str_to_int;
 	int sum = 0;
-	
+
 	count = 1;
 	while (count < argc)
 	{
